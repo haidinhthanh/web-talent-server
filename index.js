e20657903cd39535bfea6cbc18d4ce5aca03a2e1
haidinhthanh <haidt.221296.hust@gmail.com>
2020-01-09 06:45:42 +0700

    H

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use('/api/statistical', statisticalApi)
 app.get('/', (req, res) => res.send('Hello World with Express'));
 
 var CronJob = require('cron').CronJob;
-var job = new CronJob('00 33 06 * * *', function(req, res) {
+var job = new CronJob('00 48 23 * * *', function(req, res) {
     console.log("chay luc 23h 30 .........")
     console.log("chay luc 23h 30 .........")
     newspaperUtils.getFreshNewspaperFromElastic(req, res)
